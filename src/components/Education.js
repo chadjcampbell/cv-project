35d88f082a2cf3226886910edc51a839712392e1
Chad <chadjcampbell@gmail.com>
2022-11-23 14:23:22 -0500

    add container around edu's for uniformity

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,65 +7,78 @@ function Education({
   return (
     <div className="Education section">
       <h2>Education</h2>
-      {education.map((edu) => (
-        <section key={edu.id} className="eduSection">
-          <button onClick={() => deleteEducation(edu.id)} className="deleteBtn">
-            Delete
-          </button>
-          <form autoComplete="off">
-            <input
-              onChange={(event) =>
-                changeEducation(edu.id, event.target.name, event.target.value)
-              }
-              placeholder="School Name"
-              type="text"
-              name="school"
-              value={edu.school}
-            ></input>
-            <div className="dates">
-              <label htmlFor="start">Start date:</label>
+      <div className="expContainer">
+        {education.map((edu) => (
+          <section key={edu.id} className="eduSection">
+            <button
+              onClick={() => deleteEducation(edu.id)}
+              className="deleteBtn"
+            >
+              Delete
+            </button>
+            <form autoComplete="off">
               <input
                 onChange={(event) =>
                   changeEducation(edu.id, event.target.name, event.target.value)
                 }
-                type="date"
-                id="start"
-                name="start"
-                value={edu.start}
+                placeholder="School Name"
+                type="text"
+                name="school"
+                value={edu.school}
               ></input>
-            </div>
-            <div className="dates">
-              <label htmlFor="end">End date:</label>
+              <div className="dates">
+                <label htmlFor="start">Start date:</label>
+                <input
+                  onChange={(event) =>
+                    changeEducation(
+                      edu.id,
+                      event.target.name,
+                      event.target.value
+                    )
+                  }
+                  type="date"
+                  id="start"
+                  name="start"
+                  value={edu.start}
+                ></input>
+              </div>
+              <div className="dates">
+                <label htmlFor="end">End date:</label>
+                <input
+                  onChange={(event) =>
+                    changeEducation(
+                      edu.id,
+                      event.target.name,
+                      event.target.value
+                    )
+                  }
+                  type="date"
+                  id="end"
+                  name="end"
+                  value={edu.end}
+                ></input>
+              </div>
               <input
                 onChange={(event) =>
                   changeEducation(edu.id, event.target.name, event.target.value)
                 }
-                type="date"
-                id="end"
-                name="end"
-                value={edu.end}
+                placeholder="Degree Type"
+                type="text"
+                name="degree"
+                value={edu.degree}
               ></input>
-            </div>
-            <input
-              onChange={(event) =>
-                changeEducation(edu.id, event.target.name, event.target.value)
-              }
-              placeholder="Degree Type"
-              type="text"
-              name="degree"
-              value={edu.degree}
-            ></input>
-            <textarea
-              onChange={(event) =>
-                changeEducation(edu.id, event.target.name, event.target.value)
-              }
-              name="study"
-              value={edu.study}
-              placeholder="Areas of Study"
-            ></textarea>
-          </form>
-        </section>
-      ))}
+              <textarea
+                onChange={(event) =>
+                  changeEducation(edu.id, event.target.name, event.target.value)
+                }
+                name="study"
+                value={edu.study}
+                placeholder="Areas of Study"
+              ></textarea>
+            </form>
+          </section>
+        ))}
+      </div>
       <button onClick={addEducation} className="addBtn">
         Add Another School
       </button>
